932d9c1b1d185abbd39b2770a05e6a3c2aa9e9b2
Branden Rodgers <brodgers@nanigans.com>
2015-10-23 12:59:16 -0400

    idk

--- bot.js
+++ bot.js
@@ -21,23 +21,25 @@ function respond() {
   var statusCheck = /^piratebot\?/i;
   var pirateTalk = /^arr\, /i;
 
-  setTimeout(function() {
-    // statusCheck
-    if (request.text && statusCheck.test(request.text)) {
-      this.res.writeHead(200);
+  // statusCheck
+  if (request.text && statusCheck.test(request.text)) {
+    this.res.writeHead(200);
+    setTimeout(function() {
       postMessage("Ahoy Matey");
-      this.res.end(); 
-    } else if (request.text && pirateTalk.test(request.text)) {
-      this.res.writeHead(200);
+    }, 1500);
+    this.res.end(); 
+  } else if (request.text && pirateTalk.test(request.text)) {
+    this.res.writeHead(200);
+    setTimeout(function() {
       var textInPirate = talkLikeAPirate(request.text.substring(5));
       postMessage(textInPirate);
-      this.res.end();
-    } else {
-      console.log("don't care");
-      this.res.writeHead(200);
-      this.res.end();
-    }
-  }, 1500);
+    }, 1500);
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
 }
 
 function postMessage(message) {
